beea7ae9d988c7c14c639baa8463a65c2ecb0ef5
WellintonCamboim <wellinton.camboim@gmail.com>
2020-08-01 22:10:38 -0300

    Sistema de Módulos do ECMAScript

--- fundReactNative/src/App.js
+++ fundReactNative/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import Primeiro from './components/Primeiro'
+import CompOficial, { Comp1, Comp2 } from './components/Multi'
 
 // function App() {
 //     return <Text>Primeiro Componente 2</Text>
@@ -10,6 +11,9 @@ import Primeiro from './components/Primeiro'
 
 export default () => (
     <View>
+        <CompOficial />
+        <Comp1 />
+        <Comp2 />
         <Primeiro />
     </View>
 )
